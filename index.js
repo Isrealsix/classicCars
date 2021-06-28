d7ea5734d34acae60b0755b8be93a509499284e7
Isreal6ix <isrealsix@gmail.com>
2021-06-28 12:46:15 +0700

    completed the video section

--- index.js
+++ index.js
@@ -3,15 +3,23 @@ const wrappers = document.querySelectorAll('.wrapper');
 const targets = document.querySelectorAll('.target');
 
 menu.addEventListener('click', () => {
-    targets.forEach(target => {
-        target.classList.toggle('change')
-    })
-})
+	targets.forEach(target => {
+		target.classList.toggle('change');
+	});
+});
 
 wrappers.forEach(wrapper => {
-    wrapper.addEventListener('click', () => {
-        targets.forEach(target => {
-            target.classList.remove('change')
-        })
-    })
-})
+	wrapper.addEventListener('click', () => {
+		targets.forEach(target => {
+			target.classList.remove('change');
+		});
+	});
+});
+
+// Section Video
+const videos = document.querySelectorAll('.video');
+
+videos.forEach(video => {
+	video.addEventListener('mouseover', () => video.play());
+	video.addEventListener('mouseout', () => video.pause());
+});
